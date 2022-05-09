255c9daab4e0a6daabe18ceb18a946c6749178c0
User <User@LAPTOP-U1VE6DLC>
2022-05-10 01:16:11 +0400

    Add project files.

---
+++ WebApi1/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApi1.Models;
+
+namespace WebApi1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ContactController:Controller
+    {
+        [HttpGet("")]
+        [Authorize(Roles = "Admin")]
+        public List<ContactModel> Get()
+        {
+            return new List<ContactModel>()
+            {
+                new ContactModel
+                {
+                     Id = 1,
+                     FirstName ="Elvin",
+                     LastName ="Camalzade"
+                }
+            };
+        }
+    }
+}
